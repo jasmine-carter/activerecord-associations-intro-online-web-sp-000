5bdc793b9f01c5837841c25e1626e411f003b0bf
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-16 02:07:29 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,4 +1,5 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+
   def change
     create_table :songs do |t|
       t.string :name
@@ -6,5 +7,5 @@ class CreateSongs < ActiveRecord::Migration[4.2]
       t.integer :genre_id
     end
   end
-    
+
 end
